b5351418669d6d981c74fcfd212c9f91bc49f4a9
Rakesh Mahendran <rakeshmahendran99@gmail.com>
2023-06-18 02:29:02 +0530

    recent commit

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+"use client";
+
+import Link from 'next/link'
+import Image from 'next/image'
+import logo from '../public/assets/images/logo.svg'
+import {signIn , signOut , useSession , getProviders} from 'next-auth/react'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div>
+        <nav>
+            <Link href="">
+                <Image
+                 src={logo}
+                 width={30}
+                 height={30}
+                 className="object-contain"
+                />
+            </Link>
+        </nav>
+    </div>
   )
 }
 
